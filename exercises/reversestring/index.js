008e85fda44ce399ec46b6c28b4712530e749ab4
krishnamraju kammili <raju@krishnarajusMBP.lan>
2023-10-08 22:26:47 -0500

    reverse string and palindrome

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//     var rev = ''
+//     for(let i=str.length-1;i>=0; i--){
+//         rev += str[i]
+//     }
+//     return rev
+// }
 
+
+function reverse(str){
+    return str.split('').reverse().join('');
+}
+
+
+// Tip: never use classic for loop instead use of or in iterate
+function reverse1(str){
+    const reversed = ''
+    for(let character of str){
+        reversed = character + reversed
+    }
+}
+
+function reverse2(str){
+    return str.split('').reduce((reversed, character)=>{
+        return reversed + character;
+    }, '')
+}
 module.exports = reverse;
