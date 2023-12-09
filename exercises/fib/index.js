1e003d8553e1d15d193a96be59c4610ab8e0be9c
krishna <kkrajus777@gmail.com>
2023-12-09 15:18:07 -0600

    fib memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,14 +8,52 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-    if(n === 0){
-        return 0
+
+// recursive solution takes exponential run time so never use this.
+// function fib(n) {
+//     if (n < 2) {
+//         return n
+//     }
+//     return fib(n - 1) + fib(n - 2)
+// }
+
+// iterative approach linear run time.
+// function fib(n){
+//     const result = [0,1]
+
+//     for(let i=2; i<=n; i++){
+//         const a = result[i-1];
+//         const b = result[i-2];
+//         result.push(a + b)
+//     }
+//     return result[n]
+// }
+
+//memoization technique best solution
+function memoize(fn) {
+    const cache = {}
+    return function (...args) {
+        if (cache[args]) {
+            return cache[args]
+        }
+
+        const result = fn.apply(this, args)
+        cache[args] = result;
+
+        return result;
     }
-    if(n === 1){
-        return 1
-    }
-    return fib(n -1) + fib(n - 2)
 }
+
+
+function slowFib(n) {
+    if (n < 2) {
+        return n
+    }
+    return fib(n - 1) + fib(n - 2)
+}
+
+
+const fib = memoize(slowFib)
+
 
 module.exports = fib;
